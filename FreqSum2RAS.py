469d52d4d7a80ab390baa6b33e8be67338cc171f
TCLamnidis <thisseass@gmail.com>
2016-12-15 21:05:44 +0100

    Now outputting column labels only once per output.

--- FreqSum2RAS.py
+++ FreqSum2RAS.py
@@ -149,8 +149,8 @@ for i in range(2,M+1):
 #Print output tables
 print ("#FREQSUM POPS & SIZES:",*PopNames, file=args.Output, sep=" ", end="\n")
 print ("#SAMPLE POPULATION: ", Names[Test], file=args.Output, end="\n\n")
+print("RefPop","TestPop","RAS","θ-hat","θ_J" "Jackknife Error", "Allele Frequency", sep="\t", file=args.Output)
 for i in Refs:
-    print("RefPop","TestPop","RAS","θ-hat","θ_J" "Jackknife Error", "Allele Frequency", sep="\t", file=args.Output)
     for m in range(2,M+1):
         print (Names[i], Names[Test], sum(RAS[i][m]),Thetahat[i][m], ThetaJ[i][m], sqrt(Sigma2[i][m]),m, sep="\t", file=args.Output)
     print ("", file=args.Output)
